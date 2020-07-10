f3c4b56ce015d76140d9fa10d76910b07de87659
EgidioHPaixao <egidio-henrique@hotmail.com>
2020-07-10 00:28:54 -0300

    validating requirements

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,7 +10,7 @@ from . import util
 from markdown2 import Markdown
 
 class NewEntryForm(forms.Form):
-    title = forms.CharField(label="Entry title", widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    title = forms.CharField(label="Entry title", widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control col-md-8 col-lg-8', 'rows' : 10}))
     edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
 
@@ -89,7 +89,7 @@ def search(request):
     else:
         subStringEntries = []
         for entry in util.list_entries():
-            if value in entry:
+            if value.upper() in entry.upper():
                 subStringEntries.append(entry)
 
         return render(request, "encyclopedia/index.html", {
--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@
 * Install python, pip, django and markdown2
 * In the root folder, execute "python manage.py runserver"
 
+Obs
+
+if you edit and change the title, it won't delete the original file.
+
+continuar do edit
